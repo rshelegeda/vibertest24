4f41d9751a33bfedbee45ec7dfad89896d9fab6f
rshelegeda <rshelegeda>
2024-10-27 19:14:33 +0200

    five

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
-const ViberBot = require("viber-bot").Bot;
-const BotEvents = require("viber-bot").Events;
-const TextMessage = require("viber-bot").Message.Text;
-const KeyboardMessage = require("viber-bot").Message.Keyboard;
-express = require("express");
-require("dotenv").config();
-// const bodyParser = require("body-parser");
+const ViberBot = require("viber-bot").Bot,
+  BotEvents = require("viber-bot").Events,
+  TextMessage = require("viber-bot").Message.Text,
+  express = require("express");
+  require("dotenv").config();
+  const bodyParser = require('body-parser');
 
 const app = express();
 app.use(express.json());
@@ -12,139 +11,36 @@ app.use(express.json());
 if (!process.env.BOT_TOKEN) {
   console.log("Could not find bot account token key.");
   return;
-}
+};
 if (!process.env.EXPOSE_URL) {
   console.log("Could not find exposing url");
   return;
-}
+};
 
-const { phoneNumberButton, keyboard } = require("./keyboards");
+const {phoneNumberButton, keyboard} = require('./keyboards');
+
+
+
 
 const bot = new ViberBot({
-  authToken: process.env.BOT_TOKEN,
-  name: "РЕУ-8 БОТ",
-  avatar: process.env.BOT_LOGO,
+    authToken: process.env.BOT_TOKEN,
+    name: 'РЕУ-8 БОТ',
+    avatar: process.env.BOT_LOGO    
 });
 
-// bot.on(BotEvents.CONVERSATION_STARTED, (response) => {
-//   response.send(
-//     new KeyboardMessage(
-//       {
-//         Type: "keyboard",
-//         Buttons: [
-//           {
-//             Columns: 6,
-//             Rows: 1,
-//             BgColor: "#2db9b9",
-//             Text: "НАЧАТЬ ДИАЛОГ И ПОДЕЛИТЬСЯ НОМЕРОМ",
-//             ActionType: "share-phone",
-//             ActionBody: "phone",
-//           },
-//         ],
-//       },
-//       [],
-//       null,
-//       null,
-//       3
-//     )
-//   );
-// });
 
-// bot.on(BotEvents.MESSAGE_RECEIVED, (message, response) => {
-//   const roomname = response.userProfile.id;
-//   const username = response.userProfile.name;
-//   const profile_pic = response.userProfile.avatar;
-//   const country_origin = response.userProfile.country;
-//   const language_origin = response.userProfile.language;
 
-//   console.log(roomname, username, profile_pic, country_origin, language_origin);
+bot.on(BotEvents.SUBSCRIBED, (response) => {
+  response.send(
+    new TextMessage(
+      `Hi there ${response.userProfile.name}. I am ${bot.name}! Feel free to ask me anything.`
+    )
+  );
+});
 
-//   const text = message.text;
-
-//   if (message.contactPhoneNumber) {
-//     const sendPhoneNumber = async () => {
-//       try {
-//         const messageWithPhoneNumber = new TextMessage(
-//           "Здравствуйте, " +
-//             username +
-//             ". Вас приветствует РЕУ-8. Ваш номер " +
-//             message.contactPhoneNumber,
-//           keyboard
-//         );
-//         // Ваш асинхронный код здесь
-//         await response.send(messageWithPhoneNumber); // Пример асинхронного вызова
-//       } catch (error) {
-//         console.error("Ошибка при отправке сообщения:", error);
-//       }
-//     };
-//     return sendPhoneNumber();
-//   }
-
-//   // console.log(roomname, username, profile_pic, country_origin, language_origin, " ", text);
-
-//   if (text.toLowerCase() === "реу") {
-//     // Если пользователь выбрал "Да"
-//     const messageWithKeyboard = new TextMessage(
-//       "Вы выбрали Информация о РЕУ",
-//       keyboard
-//     );
-//     response.send(messageWithKeyboard);
-//   } else if (text.toLowerCase() === "квартира") {
-//     // Если пользователь выбрал "Да"
-//     const messageWithKeyboard = new TextMessage(
-//       "Вы выбрали Информация о квартире",
-//       keyboard
-//     );
-//     response.send(messageWithKeyboard);
-//   } else if (text.toLowerCase() === "оплата") {
-//     // Если пользователь выбрал "Да"
-//     const messageWithKeyboard = new TextMessage(
-//       "Вы выбрали Оплату счетов",
-//       keyboard
-//     );
-//     response.send(messageWithKeyboard);
-//   } else if (text.toLowerCase() === "телефон") {
-//     const keyboardPhone = {
-//       Type: "keyboard",
-//       Buttons: [phoneNumberButton],
-//     };
-
-//     const sendButton = async () => {
-//       try {
-//         const messageWithKeyboard = new TextMessage(
-//           "Нажмите кнопку для передачи номера телефона",
-//           keyboardPhone
-//         );
-//         // Ваш асинхронный код здесь
-//         await response.send(messageWithKeyboard); // Пример асинхронного вызова
-//       } catch (error) {
-//         console.error("Ошибка при отправке сообщения:", error);
-//       }
-//     };
-//     sendButton();
-//   } else {
-//     bot
-//       .getBotProfile()
-//       .then((response) => console.log(`Bot Named: ${response.name}`));
-//     const messageWithKeyboard = new TextMessage(
-//       "Нажмите одну из предложенных кнопок (Если Вы не видите кнопки, нажмите значек Клавиатуры в нижнем правом углу).",
-//       keyboard
-//     );
-//     response.send(messageWithKeyboard);
-//   }
-// });
-
-// bot.on(BotEvents.SUBSCRIBED, (response) => {
-//   response.send(
-//     new TextMessage(
-//       `Hi there ${response.userProfile.name}. I am ${bot.name}! Feel free to ask me anything.`
-//     )
-//   );
-// });
-
-// bot.on(BotEvents.MESSAGE_RECEIVED, (message, response) => {
-//   response.send(new TextMessage(`Message received.`));
-// });
+bot.on(BotEvents.MESSAGE_RECEIVED, (message, response) => {
+  response.send(new TextMessage(`Message received.`));
+});
 
 app.get("/", (req, res) => {
   res.send("Using global content security policy!");
